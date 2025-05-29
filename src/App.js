823354be0921e88d72564c446c9fbe1dfa317059
Sanjaystark1 <sanjaystark2004@gmail.com>
2025-05-29 17:21:33 +0530

    Initial commit: Tailwind + Study Tracker UI+added study tracker updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import React from "react";
 import StudyTracker from "./components/StudyTracker";
-function App() {
-  return (
-    <div className="App">
-      <StudyTracker />
-    </div>
-  );
-}
+
+
 const subjects = [
   {
     title: "📘 Basic Electrical Engineering",
