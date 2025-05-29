e872d8d219afce2de3f17208b75d7d00b4ab7a96
Sanjaystark1 <sanjaystark2004@gmail.com>
2025-05-29 18:24:54 +0530

    Initial commit: study tracker updated with css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
-import StudyTracker from "./components/StudyTracker";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <StudyTracker />
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex-1 p-4 overflow-y-auto">
+        <Header />
+        <Dashboard />
+      </div>
     </div>
   );
 }
-
-export default App;
